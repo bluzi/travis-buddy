69b816a54be114155d04e153c993999c8e985da3
Eliran Pe'er <eliran.peer@forter.com>
2017-11-01 01:04:44 +0200

    .

--- resolvers/mocha.js
+++ resolvers/mocha.js
@@ -1,12 +1,19 @@
+const logger = require('./../logger');
+
 module.exports = (log, params, comment) => {
     log = log.substr(log.indexOf('> mocha'));
+    logger.log(log);
     
     log = log.substr(log.indexOf('\n'));
+    logger.log(log);
 
     log = log.substr(0, log.indexOf('npm ERR!')).trim();
+    logger.log(log);
 
     log = log
             .replace('✓', '<span color="green">✓</span>');
+
+    logger.log(log);
 
     comment({
         contents: log,
