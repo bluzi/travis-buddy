75faacc7b680d220a95e76dd3dfce3a90ab3ced7
Eliran Pe'er <eliran.peer@forter.com>
2017-10-31 23:04:49 +0200

    .

--- resolvers/mocha.js
+++ resolvers/mocha.js
@@ -1,8 +1,8 @@
 const stripAnsi = require('strip-ansi');
 
 module.exports = (log, params, comment) => {
-    let mochaLog = stripAnsi(log)
-        .substr(log.indexOf('mocha'))
+    let mochaLog = log
+        .substr(log.indexOf('> mocha'))
         .substr(log.indexOf('\n') + 1)
 
     mochaLog = mochaLog.substr(0, mochaLog.indexOf('npm ERR!')).trim();
