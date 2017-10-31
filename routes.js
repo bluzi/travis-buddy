a8ef0f0b8cd60d4163728db2843e2ae06a00fff3
Eliran Pe'er <eliran.peer@forter.com>
2017-10-31 22:23:07 +0200

    add error handling

--- routes.js
+++ routes.js
@@ -10,43 +10,51 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/:mode', (req, res, next) => {
-    logger.log('\n\n!!! HANDLING NEW REQUEST !!!\n\n')
-    const payload = JSON.parse(req.body.payload);
+    try {
+        logger.log('\n\n!!! HANDLING NEW REQUEST !!!\n\n')
+        const payload = JSON.parse(req.body.payload);
 
-    logger.log(payload);
+        logger.log(payload);
 
-    if (!payload) {
-        logger.log('No payload received');
-        return res.status(500).send('No payload recived').end();
-    } else if (!payload.pull_request || !payload.pull_request_number) {
-        logger.log('Not a pull request');
-        return res.status(500).send('Not a pull request').end();
-    } else if (payload.state !== 'failed') {
-        logger.log(`State is not failed. (state: ${payload.state})`);
-        return res.status(500).send(`State is not failed. (state: ${payload.state})`).end();
+        if (!payload) {
+            logger.log('No payload received');
+            return res.status(500).send('No payload recived').end();
+        } else if (!payload.pull_request || !payload.pull_request_number) {
+            logger.log('Not a pull request');
+            return res.status(500).send('Not a pull request').end();
+        } else if (payload.state !== 'failed') {
+            logger.log(`State is not failed. (state: ${payload.state})`);
+            return res.status(500).send(`State is not failed. (state: ${payload.state})`).end();
+        }
+
+        const owner = payload.repository.owner_name;
+        const repo = payload.repository.name;
+        const mode = req.params.mode;
+        const pullRequest = payload.pull_request_number;
+        const buildNumber = payload.id;
+        const jobId = payload.matrix[0].id;
+        const author = payload.author_name;
+
+        logger.log({
+            owner,
+            repo,
+            mode,
+            pullRequest,
+            buildNumber,
+        });
+
+        logger.log(tolog);
+
+        handle(owner, repo, jobId, pullRequest, author, mode)
+            .then(() => res.status(200).send({ ok: true }).end())
+            .catch(e => {
+                logger.error(log)
+                res.status(500).end();
+            });
+    } catch (e) {
+        logger.error(e);
+        res.status(500).end();
     }
-
-    const owner = payload.repository.owner_name;
-    const repo = payload.repository.name;
-    const mode = req.params.mode;
-    const pullRequest = payload.pull_request_number;
-    const buildNumber = payload.id;
-    const jobId = payload.matrix[0].id;
-    const author = payload.author_name;
-
-    logger.log({
-        owner,
-        repo,
-        mode,
-        pullRequest,
-        buildNumber,
-    });
-
-    logger.log(tolog);
-
-    handle(owner, repo, jobId, pullRequest, author, mode)
-        .then(() => res.status(200).send({ ok: true }).end())
-        .catch(e => logger.error(log));
 });
 
 module.exports = router;
