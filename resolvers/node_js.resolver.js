a5136a0607d1ba71c3695f985e550585b2dd132c
Eliran Pe'er <eliran013@gmail.com>
2017-11-04 17:14:51 +0200

    add markdown escape and fix node resolver

--- resolvers/node_js.resolver.js
+++ resolvers/node_js.resolver.js
@@ -1,4 +1,5 @@
 const helpers = require('../utils/helpers');
+const markdownEscape = require('markdown-escape')
 
 module.exports = (log, data) => {
     return new Promise((resolve, reject) => {
@@ -7,12 +8,13 @@ module.exports = (log, data) => {
 
         scripts.forEach(script => {
             let scriptContents = log.substr(log.indexOf(script));
+            scriptContents = scriptContents.split('\n').slice(1).join('\n');
             scriptContents = scriptContents.substr(0, scriptContents.indexOf('The command')).trim();
 
             scriptContents = helpers
                 .replaceAll(scriptContents, '✓', '![alt text](https://raw.githubusercontent.com/bluzi/travis-buddy/master/resources/checkmark.png "Checkmark")');
             
-            scriptLogs[script] = scriptContents;
+            scriptLogs[script] = markdownEscape(scriptContents);
         });
 
         resolve({
