265e5d5dba861df593e9b4df5ff49de8f29ec26c
Eliran Pe'er <eliran.peer@forter.com>
2017-11-10 01:57:56 +0200

    add tests

--- handlers/test.handler.js
+++ handlers/test.handler.js
@@ -1,15 +1,16 @@
 const logger = require('../utils/logger');
 const utils = require('../utils/utils');
 const resolver = require('../resolvers/simple.resolver');
+const messageFormatter = require('../utils/message-formatter');
 
 module.exports = (language, jobId) => {
   logger.log(`Testing job '${jobId}' (${language})`);
   return new Promise((resolve, reject) => {
     utils.requestLog(jobId)
       .then(log => resolver(log, { language }))
-      .then((message) => {
-        const messages = [message];
-        return utils.formatMessage(messages, 'John Doe');
+      .then((log) => {
+        const logs = [log];
+        return messageFormatter.failure(logs, 'John Doe');        
       })
       .then(resolve)
       .catch(reject);
