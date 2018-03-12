50f9d5d32530577436f8b9a08c934759bf2e1cc5
Eliran Pe'er <eliran.peer@forter.com>
2018-03-12 20:37:13 +0200

    fix linting issue in message-formatter, parameter reassignment is forbidden

--- utils/message-formatter.js
+++ utils/message-formatter.js
@@ -2,8 +2,7 @@ const ejs = require('ejs');
 const fs = require('fs');
 
 module.exports.failure = (template, author, jobs) => {
-  if (!template) template = 'default';
-  const templateContents = fs.readFileSync(`resources/messages/failure/${template}.failure.template.md.ejs`, 'utf8');
+  const templateContents = fs.readFileSync(`resources/messages/failure/${template || 'default'}.failure.template.md.ejs`, 'utf8');
 
   return ejs.render(templateContents, {
     jobs,
@@ -12,8 +11,7 @@ module.exports.failure = (template, author, jobs) => {
 };
 
 module.exports.success = (template, author) => {
-  if (!template) template = 'default';
-  const templateContents = fs.readFileSync(`resources/messages/success/${template}.success.template.md.ejs`, 'utf8');
+  const templateContents = fs.readFileSync(`resources/messages/success/${template || 'default'}.success.template.md.ejs`, 'utf8');
 
   return ejs.render(templateContents, {
     author,
