7629961d32b36f6f209b25a3e5436fac5c105d26
Eliran Pe'er <eliran013@gmail.com>
2018-04-06 17:43:36 +0300

    refactor logger

--- utils/logger.js
+++ utils/logger.js
@@ -49,30 +49,39 @@ function formatMessage(message, meta) {
   return message;
 }
 
-function commonLogger(logdnaType, bunyanType) {
+function commonLogger({ logdna, bunyan, logzIo }) {
   return (message, meta) => {
-    if (bunyanType !== undefined && logdnaType !== undefined) {
-      if (logdnaLogger) {
-        logdnaLogger[logdnaType](formatMessage(message, meta), { meta });
-      }
-      const type = logdnaType === 'warn' ? 'warning' : logdnaType;
-      if (logzIoLogger) {
-        logzIoLogger.log({
-          type: String(type),
-          message,
-          ...meta,
-        });
-      }
-
-      bunyanLogger[bunyanType](message, meta || '');
-    }
+    if (logdnaLogger && logdna)
+      logdnaLogger[logdna](formatMessage(message, meta), { meta });
+    if (logzIoLogger && logzIo)
+      logzIoLogger.log({ type: String(logzIo), message, ...meta });
+    if (bunyan) bunyanLogger[bunyan](message);
   };
 }
 const [log, warn, error, debug] = [
-  commonLogger('log', 'info'),
-  commonLogger('warn', 'warn'),
-  commonLogger('error', 'error'),
-  commonLogger('debug', 'debug'),
+  commonLogger({
+    logdna: 'log',
+    bunyan: 'info',
+    logzIo: 'log',
+  }),
+
+  commonLogger({
+    logdna: 'warn',
+    bunyan: 'warn',
+    logzIo: 'warning',
+  }),
+
+  commonLogger({
+    logdna: 'error',
+    bunyan: 'error',
+    logzIo: 'error',
+  }),
+
+  commonLogger({
+    logdna: 'debug',
+    bunyan: 'debug',
+    logzIo: 'debug',
+  }),
 ];
 
 module.exports = {
