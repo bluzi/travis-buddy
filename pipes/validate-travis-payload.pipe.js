9644633b8c7da09bdca311796a785a90fa39fea3
Eliran Pe'er <eliran013@gmail.com>
2018-04-07 17:48:52 +0300

    improve validateTravisPayload

--- pipes/validate-travis-payload.pipe.js
+++ pipes/validate-travis-payload.pipe.js
@@ -2,15 +2,13 @@ const validateTravisPayload = async context => {
   const { payload } = context;
   let dropReason;
 
+  const allowedStates = ['failed', 'passed', 'errored'];
+
   if (!payload) {
     dropReason = 'Request dropped: No payload received';
   } else if (!payload.pull_request || !payload.pull_request_number) {
     dropReason = 'Request dropped: Not a pull request';
-  } else if (
-    payload.state !== 'failed' &&
-    payload.state !== 'passed' &&
-    payload.state !== 'errored'
-  ) {
+  } else if (allowedStates.includes(payload.state) === false) {
     dropReason = `Request dropped: Wrong state ('${payload.state}')`;
   }
 
