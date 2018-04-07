4f06180588692f796c70c6e9026e13f3f192d17c
Eliran Pe'er <eliran013@gmail.com>
2018-04-07 18:43:00 +0300

    support query parameters

--- pipes/publish.pipe.js
+++ pipes/publish.pipe.js
@@ -3,7 +3,7 @@ const createComment = require('./create-comment.pipe');
 const paipu = require('paipu');
 
 const isEditable = context =>
-  context.config.insertMode === 'update' &&
+  (context.query.insertMode || context.config.insertMode) === 'update' &&
   context.comments.some(comment => comment.user.login === context.meta.user);
 
 module.exports = paipu.pipeIf(
