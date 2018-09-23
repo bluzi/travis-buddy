b7effb72f18d5085474aeb583b9b61abbc30e87c
Eliran Pe'er <eliran013@gmail.com>
2018-09-24 00:51:03 +0300

    Release (#122)
    * Create whats-new.md
    
    * Update whats-new.md
    
    * Heroku button (#103)
    
    * add app.json
    
    * add heroku button
    
    * Add travisBuddy node in .travis.yml (#102)
    
    * Refactor code (#101)
    
    * Major refactor (#105)
    
    * refactor logger
    
    * fix configuration test
    
    * refactor all
    
    * add default config
    
    * remove unused code
    
    * update package.json
    
    * remove simple.resolver
    
    * refactor fetch-build-logs.pipe
    
    * refactor fetch-job-scripts.pipe
    
    * fix complexlity issues
    
    * improve validateTravisPayload
    
    * improve getJobCommand readability
    
    * improve getJobCommand readability
    
    * support query parameters
    
    * add context log after each pipe
    
    * add comment mode to readme doc (#104)
    
    * Fix wording of error template (#113)
    
    * add meta tags and google analytics to the website
    
    * add sentry
    
    * add regex support
    
    * Add link to Travis log #115
    
    * add debug dump
    
    * fix exit code mixups
    
    * fix logger issue with sentry
    
    * limit sentry to error
    
    * turns out regex.exec can return null
    
    * fix multiple script bug
    
    * accidently committed test changes

--- pipes/fetch-job-scripts.pipe.js
+++ pipes/fetch-job-scripts.pipe.js
@@ -2,7 +2,9 @@ const defaultScripts = require('../resources/default-scripts.json');
 
 const getCommandFromJob = (job, language) => {
   let allScripts;
-  if (job.script) {
+  if (Array.isArray(job.script)) {
+    allScripts = job.script;
+  } else if (job.script) {
     allScripts = [job.script];
   } else if (!defaultScripts[language]) {
     throw new Error(`Deafult script was not found for '${language}'`);
@@ -68,7 +70,7 @@ const getJobScripts = async (context, job) => {
       if (context.config.regex) {
         const regex = new RegExp(context.config.regex);
         const regexResult = regex.exec(scriptContents);
-        if (regexResult.length > 1) {
+        if (regexResult && regexResult.length > 1) {
           [, scriptContents] = regexResult;
         } else {
           scriptContents = undefined;
